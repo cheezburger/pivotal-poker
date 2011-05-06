6a08990fd97b307512c3bc66c1a7ee5fac45dd6a
elithompson <eli@cheezburger.com>
2011-05-06 15:51:22 -0700

    GetUnestimatedStory method

--- source/PivotalPoker/Models/IPivotal.cs
+++ source/PivotalPoker/Models/IPivotal.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using PivotalTrackerAPI.Domain.Model;
 
 namespace PivotalPoker.Models
 {
     public interface IPivotal
     {
+        PivotalStory GetUnestimatedStory();
     }
 }
--- source/PivotalPoker/Models/Pivotal.cs
+++ source/PivotalPoker/Models/Pivotal.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using PivotalTrackerAPI.Domain.Model;
 
 namespace PivotalPoker.Models
 {
     public class Pivotal : IPivotal
     {
+        private readonly PivotalUser _user;
+
+        public Pivotal()
+        {
+            var key = ConfigurationManager.AppSettings["PivotalUserAPIKey"];
+            _user = new PivotalUser(key);
+        }
+
+        public PivotalStory GetUnestimatedStory()
+        {
+            return PivotalStory.FetchStories(_user, 287145).First(ps => ps.Estimate == -1);
+        }
     }
 }
