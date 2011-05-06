e4f241b72e79cf4dad52d0eb061ae6410198738a
John Clayton <john@codemonkeylabs.com>
2011-05-06 15:51:25 -0700

    Stories have descriptions

--- source/PivotalPoker/Models/Story.cs
+++ source/PivotalPoker/Models/Story.cs
@@ -4,6 +4,8 @@ namespace PivotalPoker.Models
 {
     public class Story
     {
+        public string Description { get; set; }
+
         public long Id { get; set; }
 
         public string Name { get; set; }
