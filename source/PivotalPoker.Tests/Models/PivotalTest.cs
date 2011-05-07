6ac218cd827ca9bb57b6fd206d4514f2ca221fca
elithompson <eli@cheezburger.com>
2011-05-06 17:50:59 -0700

    more moving tests

---
+++ source/PivotalPoker.Tests/Models/PivotalTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using PivotalPoker.Models;
+
+namespace PivotalPoker.Tests
+{
+    [TestFixture]
+    class PivotalTest
+    {
+        [Test]
+        [Ignore]
+        public void CanConnectToPivotal()
+        {
+            var config = new Config();
+            var p = new Pivotal(config.Get<string>("PivotalUserAPIKey"));
+            var story = p.GetUnestimatedStory();
+            Assert.That(story, Is.Not.Null);
+        }
+
+        [Test, Ignore]
+        public void CanAssignPointsToAStory()
+        {
+            const int storyId = 13115015;
+            var config = new Config();
+            var p = new Pivotal(config.Get<string>("PivotalUserAPIKey"));
+            p.EstimateStory(storyId, 1);
+
+            var story = p.GetStory(storyId);
+
+            Assert.That(story.Estimate, Is.EqualTo(1));
+        }
+    }
+}
---
+++ source/PivotalPoker.Tests/Models/PlayerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PivotalPoker.Models;
+
+namespace PivotalPoker.Tests
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        [Test]
+        public void HashesOutToTheNamesHash()
+        {
+            const string name = "Rumples";
+            var player = new Player {Name = name};
+            Assert.That(player.GetHashCode(), Is.EqualTo(name.GetHashCode()));
+        }
+    }
+}
